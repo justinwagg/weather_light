54e27c7e4f12abd6ce42f06c211a247b36b30f14
justinwagg <jmwagg@gmail.com>
2017-10-11 21:46:21 -0400

    Added oled.py

---
+++ oled.py
@@ -0,0 +1,37 @@
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=None)
+
+disp.begin()
+disp.clear()
+disp.display()
+
+width = disp.width
+height = disp.height
+padding = -2
+top = padding
+bottom = height-padding
+
+font = ImageFont.load_default()
+
+image = Image.new('1', (width, height))
+draw = ImageDraw.Draw(image)
+
+x = 0
+def draw(conditions, high, low, avehumidity):
+	draw = ImageDraw.Draw(image)
+	draw.rectangle((0,0,width,height), outline=0, fill=0)
+	draw.text((x, top), "Monday:", font=font, fill=255)
+	draw.text((x, top+8), str(conditions) , font=font, fill=255)
+	draw.text((x, top+16), "High:" + str(high) + " Low:" + str(low), font=font, fill=255)
+	draw.text((x, top+25), "Humdity:" + str(avehumidity), font=font, fill=255)
+	disp.image(image)
+	disp.display()
+
+
